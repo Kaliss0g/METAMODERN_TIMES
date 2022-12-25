2fe1c19f741c27ca7942d6cf8ca5d6b6b4a72857
Kaliss0g <kaliss@skiff.com>
2022-12-25 03:03:52 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  staticPageGenerationTimeout: 300,
+  images: {
+    domains: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'transitivebullsh.it'
+    ],
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  },
+  experimental: {
+    //largePageDataBytes: 128 * 1000, // 128KB by default
+      largePageDataBytes: 128 * 100000,
+    },
+})
